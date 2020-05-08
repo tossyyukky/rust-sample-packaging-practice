70ee3f64d679861d51c7346088bd8b9c449fd6e8
Toshiyuki Ihara <toshiyuki.ihara@noin.tv>
2020-05-08 22:42:19 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+pub fn count(input: impl BufRead) -> HashMap<String, usize> {
+    let re = Regex::new(r"\w+").unwrap();
+    let mut freqs = HashMap::new(); //HashMap<String, usize>型
+
+    for line in input.lines() {
+        let line = line.unwrap();
+        // 4. その行を単語で分割する
+        for m in re.find_iter(&line) {
+            let word = m.as_str().to_string();
+            // 5. 出現した単語の出現頻度を数える
+            *freqs.entry(word).or_insert(0) += 1;
+
+        }
+    }
+    freqs
+}
+
+fn main() {
+    // 必要な処理
+    // 1. コマンドラインで指定された引数を読み込む
+    // 2. 指定されたファイルを開く
+    // 3. ファイルから1行ずつ読み込む
+    // 4. その行を単語で分割する
+    // 5. 出現した単語の出現頻度を数える
+
+    // 1. コマンドラインで指定された引数を読み込む
+    let filename = env::args().nth(1).expect("1 argument FILENAME reqiured");
+    // 2. 指定されたファイルを開く
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(&file);
+
+    // 3. ファイルから1行ずつ読み込む
+    let freqs = count(reader);
+    println!("{:?}", freqs);
+
+}
