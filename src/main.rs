5da0dd4b11b9eba20370acacd9a3adbaab938db9
Toshiyuki Ihara <toshiyuki.ihara@noin.tv>
2020-05-08 22:51:22 +0900

    modify: extract to lib crate

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,8 @@
-use regex::Regex;
-use std::collections::HashMap;
 use std::env;
 use std::fs::File;
-use std::io::BufRead;
 use std::io::BufReader;
 
-pub fn count(input: impl BufRead) -> HashMap<String, usize> {
-    let re = Regex::new(r"\w+").unwrap();
-    let mut freqs = HashMap::new(); //HashMap<String, usize>型
-
-    for line in input.lines() {
-        let line = line.unwrap();
-        // 4. その行を単語で分割する
-        for m in re.find_iter(&line) {
-            let word = m.as_str().to_string();
-            // 5. 出現した単語の出現頻度を数える
-            *freqs.entry(word).or_insert(0) += 1;
-
-        }
-    }
-    freqs
-}
+use wordcount::count;
 
 fn main() {
     // 必要な処理
