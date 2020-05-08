8e3a1895367d30810a11bef9d40064c5bbfee0a3
Toshiyuki Ihara <toshiyuki.ihara@noin.tv>
2020-05-08 23:45:17 +0900

    add: document comment

---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+//! wordcountはシンプルな文字、単語、行の出現頻度の計数昨日を提供します。
+//! 詳しくは[`count`](fn.count.html)関数のドキュメントを見てください。
+#![warn(missing_docs)]
+
+use regex::Regex;
+use std::collections::HashMap;
+use std::io::BufRead;
+
+/// inputから1行ずつUTF-8文字列を読み込み、頻度を数える
+///
+/// 頻度を数える対象はオプションによって制御される
+/// * [`CountOption::Char`](enum.CountOption.html#variant.Char): Unicodeの1文字ごと
+/// * [`CountOption::Word`](enum.CountOption.html#variant.Word): 正規表現 \w+ にマッチする単語ごと
+/// * [`CountOption::Line`](enum.CountOption.html#variant.Line): \n または \r\n で区切られた行ごと
+///
+/// # Panics
+///
+/// 入力がUTF-8でフォーマットされていない場合にパニックする
+pub fn count(input: impl BufRead, option: CountOption) -> HashMap<String, usize> {
+    let re = Regex::new(r"\w+").unwrap();
+    let mut freqs = HashMap::new(); //HashMap<String, usize>型
+
+    for line in input.lines() {
+        let line = line.unwrap();
+
+        use crate::CountOption::*;
+
+        // 4. その行を単語で分割する
+        match option {
+            Char => {
+                for c in line.chars() {
+                    *freqs.entry(c.to_string()).or_insert(0) += 1
+                }
+            }
+            Word => {
+                for m in re.find_iter(&line) {
+                    let word = m.as_str().to_string();
+                    // 5. 出現した単語の出現頻度を数える
+                    *freqs.entry(word).or_insert(0) += 1
+                }
+            }
+            Line => *freqs.entry(line.to_string()).or_insert(0) += 1
+        }
+    }
+    freqs
+}
+
+/// [`count`](fn.count.html)で使うオプション
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum CountOption {
+    /// 文字ごとに頻度を数える
+    Char,
+    /// 単語ごとに頻度を数える
+    Word,
+    /// 行ごとに頻度を数える
+    Line,
+}
+
+/// オプションのデフォルトは[`Word`](enum.CountOption.html]#variant.Word)
+impl Default for CountOption {
+    fn default() -> Self {
+        CountOption::Word
+    }
+}
